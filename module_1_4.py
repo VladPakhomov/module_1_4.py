96d9c05ebea7ecf81abaf1841d24a9bfbf8fba7d
VladPakhomov <mongebello@gmail.com>
2024-12-18 02:43:38 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,13 @@
+my_string = input('Как я провёл лето?-')
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
+
+
+
+
+
+
+
